b87bfa3326f415016f40fa0fc999b959ba31f574
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-03-11 01:52:08 +0100

    Email sending

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
 from .models import Post
+from django.core.paginator import Paginator
+from django.views.generic import ListView
+from .form import EmailPostForm
+from django.core.mail import send_mail
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+
+# def post_list(request):
+#     posts = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = 
+    # return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -17,3 +23,31 @@ def post_detail(request, year, month, day, post):
     )
     
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name='blog/post/list.html'
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                        f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'kelechichibuzo7@gmail.com',[cd['to']])
+            sent = True
+    
+    else:
+        form = EmailPostForm()
+    
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
