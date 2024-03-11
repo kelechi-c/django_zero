be29bcf1d96066e011746044681ed105b94cfc13
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-03-11 01:59:44 +0100

    Use env variables

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -1,5 +1,8 @@
 import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,7 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email server configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kelechichibuzo7@gmail.com'
-EMAIL_HOST_PASSWORD = 'hxawmccdeenhmqvz'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
